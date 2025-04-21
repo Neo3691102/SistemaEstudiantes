2208a703810beab0ad0a2200f8e2a16953c4cfed
Neo3691102 <nestor8772@gmail.com>
2025-04-20 20:31:43 -0600

    Clase EstudianteDAO donde se implementa el metodo listarEstudiantes para traer la informacion de la base de datos en MySQL

---
+++ src/main/java/nc/datos/EstudianteDAO.java
@@ -0,0 +1,53 @@
+package nc.datos;
+
+import nc.dominio.Estudiante;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import static nc.conexion.Conexion.getConexion;
+
+//DAO - Data Access Object
+public class EstudianteDAO {
+
+    public List<Estudiante> listarEstudiantes(){
+        List<Estudiante> estudiantes = new ArrayList<>();
+        PreparedStatement ps; //ayuda a preparar la sentencia SQL que lanzaremos a la BD
+        ResultSet rs; //ayuda a manejar el resultado obtenido de la base de datos
+        Connection con = getConexion(); // objeto para conectarse a la base de datos
+        String sql =  "SELECT * FROM estudiante ORDER BY id_estudiante";
+        try{
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery(); // ejecuta el query y almacena resultado de la base de datos
+            while(rs.next()){//next() itera cada uno de los registros
+                var estudiante = new Estudiante();
+                estudiante.setIdEstudiante(rs.getInt("id_estudiante"));
+                estudiante.setNombre(rs.getString("nombre"));
+                estudiante.setApellido(rs.getString("apellido"));
+                estudiante.setTelefono(rs.getString("telefono"));
+                estudiante.setEmail(rs.getString("email"));
+                estudiantes.add(estudiante);
+            }
+        } catch (Exception e) {
+            System.out.println("Ocurrio un error al seleccionar datos: " + e.getMessage());
+        }
+        finally{
+            try{
+                con.close();
+            }catch(Exception e){
+                System.out.println("Ocurrio un error al cerrar conexion" + e.getMessage());
+            }
+        }
+        return estudiantes;
+    }
+
+    public static void main(String[] args) {
+        var estudianteDao = new EstudianteDAO();
+        System.out.println("Listado de estudiantes: ");
+        List<Estudiante> estudiantes = estudianteDao.listarEstudiantes();
+        estudiantes.forEach(System.out::println);
+    }
+}
